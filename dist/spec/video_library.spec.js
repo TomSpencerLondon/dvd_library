f8c60f106de1de04ed50198b44812916fe8f2689
Tom <thomas.spencer@codurance.com>
2020-08-25 07:09:11 +0100

    Refactor test for documentation

--- dist/spec/video_library.spec.js
+++ dist/spec/video_library.spec.js
@@ -2,12 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var video_library_1 = require("../video_library");
 describe('Video Library', function () {
-    it('accepts donated DVD', function () {
-        var library = [];
-        var dvd = { title: 'The Abyss', copies: 0 };
-        library = video_library_1.donate(library, dvd);
+    var library = [];
+    var dvd = { title: 'The Abyss', copies: 0 };
+    library = video_library_1.donate(library, dvd);
+    it('adds donated DVD to library', function () {
         var index = library.map(function (dvd) { return dvd.title; }).indexOf('The Abyss');
         expect(index).toBe(0);
+    });
+    it('assigns 1 copy to donated DVD', function () {
         expect(library[0].copies).toBe(1);
     });
 });
--- spec/video_library.spec.ts
+++ spec/video_library.spec.ts
@@ -1,12 +1,17 @@
 import {donate } from '../video_library';
 
 describe('Video Library', () => {
-  it('accepts donated DVD', () => {
-    let library = [];
-    const dvd = {title: 'The Abyss', copies: 0};
-    library = donate(library, dvd);
+  let library = [];
+  const dvd = {title: 'The Abyss', copies: 0};
+  library = donate(library, dvd);
+
+
+  it('adds donated DVD to library', () => {
     let index = library.map((dvd) => dvd.title).indexOf('The Abyss');
     expect(index).toBe(0);
-    expect(library[0].copies).toBe(1);
   });
+
+  it('assigns 1 copy to donated DVD', () => {
+    expect(library[0].copies).toBe(1);
+  })
 });
